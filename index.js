0d9f21f2184d8ad29c1ac2f4d8ab8a2d77846816
zubairahmed22 <zubairwastibaloch@gmail.com>
2024-10-23 03:02:13 -0700

    frontend url added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     next()
   }) 
 
-app.use(cors({ credentials: true, origin:'http://localhost:3000'}))
+app.use(cors({ credentials: true, origin:'https://soft-gumption-4ae84e.netlify.app'}))
 app.use(express.json())
 app.use(cookieParser())
 
