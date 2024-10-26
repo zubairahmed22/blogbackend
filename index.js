19b15c20663b18399d9776b9e07192feb9d05129
zubairahmed22 <zubairwastibaloch@gmail.com>
2024-10-26 08:30:14 -0700

    header added

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ app.use(cors({
     credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://soft-gumption-4ae84e.netlify.app"); // the link of my front-end app on Netlify
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    res.setHeader('content-type', 'application/json');
+    next();
+  });
 
 // app.use('/uploads', express.static(__dirname + '/uploads'));
 const salt = bcrypt.genSaltSync(10);
