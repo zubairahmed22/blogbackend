523ce1ae2a8944a23a6b48e3734dc5d59a4feb79
zubairahmed22 <zubairwastibaloch@gmail.com>
2024-10-27 07:14:42 -0700

    previous cors removed

--- index.js
+++ index.js
@@ -26,19 +26,7 @@ app.use(cors({
     credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://soft-gumption-4ae84e.netlify.app"); // the link of my front-end app on Netlify
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    res.setHeader('content-type', 'application/json');
-    next();
-  });
+
 
 // app.use('/uploads', express.static(__dirname + '/uploads'));
 const salt = bcrypt.genSaltSync(10);
@@ -75,6 +63,7 @@ app.post('/api/login', async(req,res) =>{
    
    if(passOkdata){
    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        console.log(token)
         if (err) throw err;
         res.cookie('token', token).json({
           id:userDoc._id,
